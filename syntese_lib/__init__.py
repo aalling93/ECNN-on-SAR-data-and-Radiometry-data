3fd685cc780269de699cafea3eab6ce19cfb52d6
Kristian Sørensen <kristian.sorensen@hotmail.com>
2020-07-24 17:38:38 +0200

    added init

---
+++ syntese_lib/__init__.py
@@ -0,0 +1,33 @@
+
+
+
+
+from .my_tools import satf
+from .my_tools import img_stretch
+from .my_tools import img_normalize
+from .my_tools import color_img
+from .my_tools import file_paths
+from .my_tools import run_gpt_model
+
+
+from .my_s3 import get_img
+from .my_s3 import rotate_points_s3
+
+from .my_s1 import s1_show
+from .my_s1 import download_sentinel1
+from .my_s1 import get_img
+from .my_s1 import get_subsets
+
+
+from .my_modis import modistoshape
+from .my_modis import plotfire_shp
+from .my_modis import get_fires
+
+
+from .my_cnn import s1_model
+from .my_cnn import s3_model
+from .my_cnn import training_CNN
+from .my_cnn import prediction_image
+from .my_cnn import predictions_folder
+from .my_cnn import make_dataset
+from .my_cnn import make_trainig_areas
